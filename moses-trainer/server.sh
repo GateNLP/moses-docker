144462cbefbe41be60d02c44a9a4f417c04262a9
Adam Funk <a.funk@sheffield.ac.uk>
2019-05-15 17:05:18 +0100

    full path

--- moses-trainer/server.sh
+++ moses-trainer/server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # default port 8080
-/home/moses/mosesdecoder/bin/moses -f /data/model/mert-work/moses.ini --server &
+/home/moses/mosesdecoder/bin/moses -f /home/moses/model/working/train/model/moses.ini --server &
 
 # wrapper port 8081
 /home/moses/server-wrapper.py &
